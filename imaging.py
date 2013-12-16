c827d4272c90943d3e9ee93d72531ae2b341f497
Shruthi <shruthisalkar@gmail.com>
2013-12-17 00:36:37 +0530

    color not changing

--- imaging.py
+++ imaging.py
@@ -2,17 +2,37 @@ import sys, Image, ImageDraw
 
 inputImage = Image.open("img.jpg")
 
+white = (255, 255, 255)
+black = (0, 0, 0)
 
-txt = "Not really a fancy text!"
-size = (1150, 550)
-color = (0, 225, 0)
-img = Image.new('RGB', size, color)
-imgDrawer = ImageDraw.Draw(img)
-imgDrawer.text((15, 120), txt)
-#img.show()
+size = width, height = inputImage.size
+change = x, y, z = 255, 0, 0
 
 inputImage.save("output.jpg")
-outputImage = Image.open("output.jpg").resize(size)
-outputImage.resize(size)
+outputImage = Image.open("output.jpg")
+img = outputImage.load()
+
+#Making all white-like pixels = (255, 255, 255) and black-like pixels = (0, 0, 0)
+for i in range(0, height):
+	for j in range(0, width):
+		p, q, r = img[i,j]
+		if p<75 and q<75 and r<75:
+			img[i,j] = black
+		else:
+			img[i,j] = white
+
+#Algorithm
+for i in range(0, height):
+	change = x, y, z = 255, 0, 0
+	for j in range(0, width):
+		if img[i,j] == black:
+			while img[i,j] == black:
+				img[i,j] = change
+				j = j+1
+			if x==255: x, y = 0, 255
+			if y==255: y, z = 0, 255
+			if z==255: z, x = 0, 255
+
+
 outputImage.show()
 outputImage.save("output.jpg")
